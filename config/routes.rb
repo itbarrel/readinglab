c82914bbca56e741be937c616623c6672990ef27
Mashal Zareen <mishalzareen153@gmail.com>
2022-11-10 18:22:10 +0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web => '/sidekiq'
   resources :accounts 
+  resources :vacations 
+
 
   # Defines the root path route ("/")
   root 'pages#home'
