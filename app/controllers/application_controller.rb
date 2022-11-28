8b1df235c92a9b8780bb7e69031a6a0469e492fd
Saad Mahmood <mr-saaad07@gmail.com>
2022-11-28 18:15:11 +0500

    sidebar settings falcon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,30 +6,30 @@ class ApplicationController < ActionController::Base
   before_action :generate_sidebar, unless: :json_request?
 
   def generate_sidebar
-    @menu_list = [
-      { url: '/', text: 'Dashboard', class: '', icon: 'micon bi bi-house', sub_items: [] },
-      { url: '/calendar', text: 'Calendar', class: '', icon: 'micon bi bi-calendar4-week', sub_items: [] },
-      { text: 'Registration', class: '', icon: 'micon bi bi-basket', sub_items: [
-        { url: '/parents', text: 'Parents', class: '', icon: 'micon bi bi-people' },
-        { url: '/students', text: 'Student Listing', class: '', icon: 'micon fa fa-plane' },
-        { url: '/interviews', text: 'Interviews', class: '', icon: 'micon fa fa-group' }
+    @menu_list = {
+      'General': [
+        { url: '/calendar', text: 'Calendar', class: '', icon: 'fas fa-calendar-alt', sub_items: [] },
+        { text: 'Registration', class: '', icon: 'fab fa-trello', sub_items: [
+          { url: '/parents', text: 'Parents', class: '', icon: 'micon bi bi-people' },
+          { url: '/students', text: 'Student Listing', class: '', icon: 'micon fa fa-plane' },
+          { url: '/interviews', text: 'Interviews', class: '', icon: 'micon fa fa-group' }
 
-      ] },
-      { text: 'Classes', class: '', icon: 'micon bi bi-basket', sub_items: [
-        { url: '/klasses', text: 'Active', class: '', icon: 'micon bi bi-book' },
-        { url: '/rooms', text: 'Obselote', class: '', icon: 'micon fa fa-bank' }
+        ] },
+        { text: 'Classes', class: '', icon: 'micon bi bi-basket', sub_items: [
+          { url: '/klasses', text: 'Active', class: '', icon: 'micon bi bi-book' },
+          { url: '/rooms', text: 'Obselote', class: '', icon: 'micon fa fa-bank' }
 
-      ] },
-      { url: '/communication', text: 'Communication', class: '', icon: 'micon bi bi-calendar4-week', sub_items: [] },
-
-      { text: 'Settings', class: '', icon: 'micon bi bi-basket', sub_items: [
+        ] },
+        { url: '/communication', text: 'Communication', class: '', icon: 'micon bi bi-calendar4-week', sub_items: [] }
+      ],
+      'Settings': [
         { url: '/klass_templates', text: 'Class tempelate ', class: '', icon: 'micon bi bi-book' },
-        { url: '/rooms', text: 'rooms', class: '', icon: 'micon fa fa-bank' },
+        { url: '/rooms', text: 'Rooms', class: '', icon: 'micon fa fa-bank' },
         { url: '/staffs', text: 'Staff', class: '', icon: 'micon bi bi-people' },
         { url: '/vacations', text: 'Vacations', class: '', icon: 'micon fa fa-plane' },
         { url: '/profile', text: 'Profile', class: '', icon: 'micon bi bi-person' }
-      ] }
-    ]
+      ]
+    }
   end
 
   private
