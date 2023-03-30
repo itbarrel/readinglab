ac222b5706832f35c288201e2962f7b91baa4916
Mashal Zareen <mishalzareen153@gmail.com>
2023-03-30 15:57:51 +0500

    old db model

--- app/models/old/account.rb
+++ app/models/old/account.rb
@@ -1,5 +1,36 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id                   :uuid             not null, primary key
+#  active               :boolean          default(TRUE)
+#  address              :text
+#  billing_scheme       :string
+#  country_code         :string
+#  currency             :string
+#  default_meeting_form :uuid
+#  default_student_form :uuid
+#  deleted_at           :datetime
+#  email                :string
+#  mobile               :string
+#  name                 :string
+#  postal_code          :integer
+#  province             :string
+#  send_emails          :boolean          default(TRUE)
+#  settings             :jsonb
+#  timezone             :string
+#  created_at           :datetime
+#  updated_at           :datetime
+#  parent_id            :uuid
+#  user_id              :uuid
+#
+# Indexes
+#
+#  index_accounts_on_parent_id  (parent_id)
+#  index_accounts_on_user_id    (user_id)
+#
 class Old::Account < Old::ApplicationRecord
   has_many :students, class_name: 'Old::Student'
   has_many :users, class_name: 'Old::User'
