45c8734765e65529711c4c80063358d5e1ca24ed
Saaad Mahmood <saad.mahmood@gmail.com>
2023-05-06 16:34:51 +0500

    database sync

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_13_103443) do
     t.string "grade"
     t.datetime "deleted_at"
     t.uuid "account_id", null: false
-    t.uuid "klass_id", null: false
+    t.uuid "klass_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["account_id", "name", "deleted_at"], name: "book_name", unique: true
@@ -90,7 +90,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_13_103443) do
 
   create_table "form_details", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.jsonb "form_values"
-    t.uuid "user_id", null: false
+    t.uuid "user_id"
     t.uuid "form_id", null: false
     t.uuid "account_id", null: false
     t.string "parent_type", null: false
@@ -375,8 +375,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_13_103443) do
     t.integer "status"
     t.uuid "account_id", null: false
     t.uuid "student_id", null: false
-    t.uuid "klass_id", null: false
-    t.uuid "book_id", null: false
+    t.uuid "klass_id"
+    t.uuid "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
