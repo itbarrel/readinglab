a76c0e0297cd5ad779eac6743b7839b83c11d42d
Saaad Mahmood <saad.mahmood@gmail.com>
2023-05-19 14:59:44 +0500

    calendar klass permission sheet enforce

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,35 +12,40 @@ class Ability
 
     can :manage, :all if user.super_admin?
 
-    return unless user.admin?
+    if user.admin?
+      can :manage, Book, account_id: user.account_id
+      can :manage, City, account_id: user.account_id
+      can :manage, ContentLibrary, account_id: user.account_id
+      can :manage, Form, account_id: user.account_id
+      can :manage, FormField, account_id: user.account_id
+      can :manage, Interview, account_id: user.account_id
+      can :manage, KlassTemplate, account_id: user.account_id
+      can :manage, Klass, account_id: user.account_id
+      can :manage, Meeting, account_id: user.account_id
+      can :manage, MessageTemplate, account_id: user.account_id
+      can :manage, Parent, account_id: user.account_id
+      can :manage, ReceiptType, account_id: user.account_id
+      can :manage, Receipt, account_id: user.account_id
+      can :manage, Room, account_id: user.account_id
+      can :manage, Student, account_id: user.account_id
+      can :manage, User, account_id: user.account_id
+      can :manage, TrajectoryDetail, account_id: user.account_id
+      can :manage, VacationType, account_id: user.account_id
+      can :manage, Vacation, account_id: user.account_id
+      can :manage, StudentMeeting, account_id: user.account_id
 
-    can :manage, Book, account_id: user.account_id
-    can :manage, City, account_id: user.account_id
-    can :manage, ContentLibrary, account_id: user.account_id
-    can :manage, Form, account_id: user.account_id
-    can :manage, FormField, account_id: user.account_id
-    can :manage, Interview, account_id: user.account_id
-    can :manage, KlassTemplate, account_id: user.account_id
-    can :manage, Klass, account_id: user.account_id
-    can :manage, Meeting, account_id: user.account_id
-    can :manage, MessageTemplate, account_id: user.account_id
-    can :manage, Parent, account_id: user.account_id
-    can :manage, ReceiptType, account_id: user.account_id
-    can :manage, Receipt, account_id: user.account_id
-    can :manage, Room, account_id: user.account_id
-    can :manage, Student, account_id: user.account_id
-    can :manage, User, account_id: user.account_id
-    can :manage, TrajectoryDetail, account_id: user.account_id
-    can :manage, VacationType, account_id: user.account_id
-    can :manage, Vacation, account_id: user.account_id
-    can :manage, StudentMeeting, account_id: user.account_id
+      can :manage, FormField, { form: { account_id: user.account_id } }
 
-    can :manage, FormField, { form: { account_id: user.account_id } }
+      can :manage, StudentClass, { student: { account_id: user.account_id }, klass: { account_id: user.account_id } }
+      can :manage, KlassForm, { klass: { account_id: user.account_id }, form: { account_id: user.account_id } }
+      can :manage, StudentForm, { student: { account_id: user.account_id }, form: { account_id: user.account_id } }
+      can :manage, Payment,
+          { student: { account_id: user.account_id }, meeting: { account_id: user.account_id }, receipt: { account_id: user.account_id } }
+    end
 
-    can :manage, StudentClass, { student: { account_id: user.account_id }, klass: { account_id: user.account_id } }
-    can :manage, KlassForm, { klass: { account_id: user.account_id }, form: { account_id: user.account_id } }
-    can :manage, StudentForm, { student: { account_id: user.account_id }, form: { account_id: user.account_id } }
-    can :manage, Payment,
-        { student: { account_id: user.account_id }, meeting: { account_id: user.account_id }, receipt: { account_id: user.account_id } }
+    return unless user.teacher?
+
+    can :manage, Klass, { teacher_id: user.id, klass: { account_id: user.account_id } }
+    can :manage, Meeting, { klass: { teacher_id: user.id, account_id: user.account_id } }
   end
 end
