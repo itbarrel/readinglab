f3f113300609fea36c885c9c06ea0e247e84c7e8
Saaad Mahmood <saad.mahmood@gmail.com>
2023-05-19 20:19:22 +0500

    super visor permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,64 @@
 class Ability
   include CanCan::Ability
 
+  def admin_permissions_for(user)
+    can :manage, Book, account_id: user.account_id
+    can :manage, City, account_id: user.account_id
+    can :manage, ContentLibrary, account_id: user.account_id
+    can :manage, Form, account_id: user.account_id
+    can :manage, FormField, account_id: user.account_id
+    can :manage, Interview, account_id: user.account_id
+    can :manage, KlassTemplate, account_id: user.account_id
+    can :manage, Klass, account_id: user.account_id
+    can :manage, Meeting, account_id: user.account_id
+    can :manage, MessageTemplate, account_id: user.account_id
+    can :manage, Parent, account_id: user.account_id
+    can :manage, ReceiptType, account_id: user.account_id
+    can :manage, Receipt, account_id: user.account_id
+    can :manage, Room, account_id: user.account_id
+    can :manage, Student, account_id: user.account_id
+    can :manage, User, account_id: user.account_id
+    can :manage, TrajectoryDetail, account_id: user.account_id
+    can :manage, VacationType, account_id: user.account_id
+    can :manage, Vacation, account_id: user.account_id
+    can :manage, StudentMeeting, account_id: user.account_id
+
+    can :manage, FormField, { form: { account_id: user.account_id } }
+
+    can :manage, StudentClass, { student: { account_id: user.account_id }, klass: { account_id: user.account_id } }
+    can :manage, KlassForm, { klass: { account_id: user.account_id }, form: { account_id: user.account_id } }
+    can :manage, StudentForm, { student: { account_id: user.account_id }, form: { account_id: user.account_id } }
+    can :manage, Payment,
+        { student: { account_id: user.account_id }, meeting: { account_id: user.account_id }, receipt: { account_id: user.account_id } }
+  end
+
+  def supervisor_permissions_for(user)
+    can :manage, Book, account_id: user.account_id
+    can :manage, City, account_id: user.account_id
+    can :manage, ContentLibrary, account_id: user.account_id
+    can :manage, Form, account_id: user.account_id
+    can :manage, FormField, account_id: user.account_id
+    can :manage, Interview, account_id: user.account_id
+    can :manage, KlassTemplate, account_id: user.account_id
+    can :manage, Klass, account_id: user.account_id
+    can :manage, Meeting, account_id: user.account_id
+    can :manage, MessageTemplate, account_id: user.account_id
+    can :manage, Parent, account_id: user.account_id
+    can :manage, Room, account_id: user.account_id
+    can :manage, Student, account_id: user.account_id
+    can :manage, User, account_id: user.account_id
+    can :manage, TrajectoryDetail, account_id: user.account_id
+    can :manage, VacationType, account_id: user.account_id
+    can :manage, Vacation, account_id: user.account_id
+    can :manage, StudentMeeting, account_id: user.account_id
+
+    can :manage, FormField, { form: { account_id: user.account_id } }
+
+    can :manage, StudentClass, { student: { account_id: user.account_id }, klass: { account_id: user.account_id } }
+    can :manage, KlassForm, { klass: { account_id: user.account_id }, form: { account_id: user.account_id } }
+    can :manage, StudentForm, { student: { account_id: user.account_id }, form: { account_id: user.account_id } }
+  end
+
   def initialize(user)
     # Handle the case where we don't have a current_user i.e. the user is a guest
     # user ||= User.new
@@ -12,36 +70,9 @@ class Ability
 
     can :manage, :all if user.super_admin?
 
-    if user.admin?
-      can :manage, Book, account_id: user.account_id
-      can :manage, City, account_id: user.account_id
-      can :manage, ContentLibrary, account_id: user.account_id
-      can :manage, Form, account_id: user.account_id
-      can :manage, FormField, account_id: user.account_id
-      can :manage, Interview, account_id: user.account_id
-      can :manage, KlassTemplate, account_id: user.account_id
-      can :manage, Klass, account_id: user.account_id
-      can :manage, Meeting, account_id: user.account_id
-      can :manage, MessageTemplate, account_id: user.account_id
-      can :manage, Parent, account_id: user.account_id
-      can :manage, ReceiptType, account_id: user.account_id
-      can :manage, Receipt, account_id: user.account_id
-      can :manage, Room, account_id: user.account_id
-      can :manage, Student, account_id: user.account_id
-      can :manage, User, account_id: user.account_id
-      can :manage, TrajectoryDetail, account_id: user.account_id
-      can :manage, VacationType, account_id: user.account_id
-      can :manage, Vacation, account_id: user.account_id
-      can :manage, StudentMeeting, account_id: user.account_id
+    set_admin_with user if user.admin?
 
-      can :manage, FormField, { form: { account_id: user.account_id } }
-
-      can :manage, StudentClass, { student: { account_id: user.account_id }, klass: { account_id: user.account_id } }
-      can :manage, KlassForm, { klass: { account_id: user.account_id }, form: { account_id: user.account_id } }
-      can :manage, StudentForm, { student: { account_id: user.account_id }, form: { account_id: user.account_id } }
-      can :manage, Payment,
-          { student: { account_id: user.account_id }, meeting: { account_id: user.account_id }, receipt: { account_id: user.account_id } }
-    end
+    supervisor_permissions_for user if user.supervisor?
 
     return unless user.teacher?
 
