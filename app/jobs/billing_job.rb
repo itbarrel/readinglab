6b412970236680e493cea7ad7d29978a52c5fe18
Mashal Zareen <mishalzareen153@gmail.com>
2023-07-18 19:02:13 +0500

    RL-18: sent emails to parents

--- app/jobs/billing_job.rb
+++ app/jobs/billing_job.rb
@@ -9,8 +9,10 @@ class BillingJob
     students = Student.all
     students.each do |student|
       next_billing_date = student.next_billing_date
-      days_until_next_billing = (next_billing_date - Time.today).to_i
-      ParentMailer.billing(student).deliver_now if days_until_next_billing == 7
+      if next_billing_date.present?
+        days_until_next_billing = (next_billing_date - Time.today).to_i
+        BillingMailer.mailer(student).deliver_now if days_until_next_billing == 7 || 1.day
+      end
     end
   end
 end
