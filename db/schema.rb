3b8ebb2b06a1ade5c03578d0ce84fafde1b787b0
azeemakmal <azeem_akmal@outlook.com>
2023-07-25 19:32:23 +0500

    paper trail gem add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_10_100434) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_25_105404) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -79,7 +79,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_100434) do
     t.string "grade"
     t.datetime "deleted_at"
     t.uuid "account_id", null: false
-    t.uuid "klass_id"
+    t.uuid "klass_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["account_id", "name", "deleted_at"], name: "book_name", unique: true
@@ -118,7 +118,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_100434) do
 
   create_table "form_details", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.jsonb "form_values"
-    t.uuid "user_id"
+    t.uuid "user_id", null: false
     t.uuid "form_id", null: false
     t.uuid "account_id", null: false
     t.string "parent_type", null: false
@@ -435,8 +435,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_100434) do
     t.integer "status"
     t.uuid "account_id", null: false
     t.uuid "student_id", null: false
-    t.uuid "klass_id"
-    t.uuid "book_id"
+    t.uuid "klass_id", null: false
+    t.uuid "book_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
@@ -505,6 +505,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_100434) do
     t.index ["account_id", "name", "deleted_at"], name: "vacations_name", unique: true
     t.index ["account_id"], name: "index_vacations_on_account_id"
     t.index ["vacation_type_id"], name: "index_vacations_on_vacation_type_id"
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.uuid "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object"
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "accounts", "account_types"
