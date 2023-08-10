fada2852507ebc4ee33773b31d94abb5a15088dd
azeemakmal <azeem_akmal@outlook.com>
2023-08-10 13:02:20 +0500

    work on disable_field

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -8,13 +8,13 @@ module FormHelper
       value = data.form_values[field.model_key]
     else
       value = nil
-      data = { submitted: false }.to_dot
+      data = { created_at: Time.zone.now }.to_dot
     end
 
-    field_disabled = data.submitted && !current_user.admin?
+    field_disabled = data.created_at < 2.weeks.ago && !(current_user.admin? || current_user.supervisor?)
 
     case field.field_type
-    when 'text_field'
+    when 'text_field'current_user.admin?
       text_field_tag model_key, value, class: 'form-control', required: field.necessary, disabled: field_disabled
     when 'number_field'
       number_field_tag model_key, value, class: 'form-control', required: field.necessary, disabled: field_disabled
