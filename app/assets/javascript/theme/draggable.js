3b8d75a23adbf80773a67852e5e33e454054b482
azeemakmal <azeem_akmal@outlook.com>
2023-08-16 16:04:34 +0500

    refresh not needed on class edit

--- app/assets/javascript/theme/draggable.js
+++ app/assets/javascript/theme/draggable.js
@@ -4,6 +4,53 @@
 
 import utils from '../utils';
 
+const drag_start = ()=>{}
+const drag_stop = ({ data })=>{
+  setTimeout(()=>{
+    const { source: el } = data
+    const klass_id = $('#klass_id').attr('name')
+    const student_class_id = data.source.id
+    const klass_form_id = data.originalSource.parentNode.id
+    const sourceContainerId = data.sourceContainer.id
+
+    if (klass_form_id == sourceContainerId) return
+
+    const student_form = { klass_form_id }
+    const student_forms_attributes = []
+
+    if(klass_form_id == 'unassigned'){
+      student_forms_attributes.push({
+        id: student_class_id,
+        _destroy: true
+      })
+    } else {
+      if (sourceContainerId == 'unassigned'){
+        student_forms_attributes.push({
+          klass_form_id, student_class_id
+        })
+      } else {
+        student_forms_attributes.push({
+          id: student_class_id,
+          _destroy: true
+        })
+        student_forms_attributes.push({
+          klass_form_id,
+          student_class_id: data.originalSource.attributes['data-scid'].nodeValue
+        })
+      }
+    }
+
+    if(klass_id && student_class_id){
+      $.ajax({
+        url: `/klasses/${klass_id}`,
+        method: 'PUT',
+        dataType: 'script',
+        data: { klass: { student_forms_attributes }}
+      });
+    }
+  }, 0)
+}
+
 window.draggableInit = (options = {}) => {
   const Selectors = {
     BODY: 'body',
@@ -45,10 +92,10 @@ window.draggableInit = (options = {}) => {
     });
 
     // Hide form when drag start
-    sortable.on(Events.DRAG_START, options.drag_start);
+    sortable.on(Events.DRAG_START, (options.drag_start || drag_start));
 
     // Place forms and other contents bottom of the sortable container
-    sortable.on(Events.DRAG_STOP, options.drag_stop);
+    sortable.on(Events.DRAG_STOP, (options.drag_stop || drag_stop));
   }
 };
 
